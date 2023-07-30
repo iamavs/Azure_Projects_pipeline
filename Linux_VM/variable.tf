42209fa7cb76abfa377e1822287d201f58e96b2e
lakshmikanth <lakshmikanthisl@outlook.com>
2023-07-30 13:17:41 +0530

    Adding Resource Group File

---
+++ Linux_VM/variable.tf
@@ -0,0 +1,11 @@
+variable "rg_name" {
+    description = "Resource Group Name"
+    type = string
+    default = "myrg1"
+}
+
+variable "rg_location" {
+    description = "Resource Group Location"
+    type = string
+    default = "East US"
+}
