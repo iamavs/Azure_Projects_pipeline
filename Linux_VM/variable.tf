eac3ea7b31f99186dcdc3a93166b7b43a32b2add
lakshmikanth <lakshmikanthisl@outlook.com>
2023-07-30 15:00:03 +0530

    Adding Virtual Network files

--- Linux_VM/variable.tf
+++ Linux_VM/variable.tf
@@ -9,3 +9,40 @@ variable "rg_location" {
     type = string
     default = "East US"
 }
+
+variable "vnet_name" {
+    description = "Virtual Netwrok Name"
+    type = string
+    default = "myvnet1"
+}
+
+variable "vnet_address" {
+    description = "Virtual Address CIDR"
+    type = list(string)
+    default = [ "10.0.0.0/16" ]
+}
+
+variable "subnet_name" {
+    description = "Subnet Name"
+    type = string
+    default = "mysubnet1"
+}
+
+variable "subnet_address" {
+    description = "Subnet Address Space"
+    type = list(string)
+    default = [ "10.0.0.0/24" ] 
+}
+
+variable "pip_name" {
+    description = "Public IP Name"
+    type = string
+    default = "mypip1"
+}
+
+variable "nic_name" {
+    description = "Network Interface Name"
+    type = string
+    default = "mynic1"
+}
+
