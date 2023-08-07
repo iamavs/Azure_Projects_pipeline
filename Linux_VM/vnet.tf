e39c20ef7f5cf00d532c69f8b71e62d19efe6a0e
lakshmikanth <lakshmikanthisl@outlook.com>
2023-08-07 11:37:16 +0530

    VM using Count

--- Linux_VM/vnet.tf
+++ Linux_VM/vnet.tf
@@ -24,6 +24,7 @@ resource "azurerm_public_ip" "mypip" {
 }
 
 resource "azurerm_network_interface" "mynic" {
+  count = var.no_instances
   name                = "{var.nic_name}-${count.index +1}"
   location            = azurerm_resource_group.myrg.location
   resource_group_name = azurerm_resource_group.myrg.name
