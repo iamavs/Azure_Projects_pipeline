f499dd4e8e2b9dc5a8cc98a3905a1dda4b270cd0
lakshmikanth <lakshmikanthisl@outlook.com>
2023-08-09 21:21:47 +0530

    Changing Public IP Sku

--- Linux_VM/lb.tf
+++ Linux_VM/lb.tf
@@ -4,6 +4,7 @@ resource "azurerm_public_ip" "mypip2" {
   location            = azurerm_resource_group.myrg.location
   resource_group_name = azurerm_resource_group.myrg.name
   allocation_method   = "Static"
+  sku = "Standard"
   depends_on = [ azurerm_resource_group.myrg ]
 }
 
